c48ac4a5b2a88f010c2a0a946919cba0ccbdffc9
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-03 15:43:51 +0530

    42. Passing and Receiving Props

--- src/index.js
+++ src/index.js
@@ -58,15 +58,38 @@ function App() {
     );
 }
 
-//create component 
-function Pizza() {
+function Menu() {
     return (
-        <div>
-            <img src="pizzas/spinaci.jpg" alt="pizza spinaci"/>
-            <h3>Pizza Spinaci</h3>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        </div>
-             
+        <main className="menu">
+            <h2>Our Menu</h2>
+            <Pizza 
+                name="Pizza Spinaci" 
+                ingredient="Tomato, mozarella, spinach, and ricotta cheese" 
+                photoName="pizzas/spinaci.jpg" 
+                price={10}
+            />
+             <Pizza 
+                name="Pizza Funghi" 
+                ingredient="Tomato, mozarella, mushrooms, and onion" 
+                photoName="pizzas/funghi.jpg" 
+                price={12}
+            />
+        </main>
+    );
+}
+
+//create component 
+function Pizza(props) {
+    console.log(props);
+    return (
+      <div className="pizza">
+         <img src={props.photoName} alt={props.name} />
+         <div>
+            <h3>{props.name}</h3>
+            <p>{props.ingredient}</p>
+            <span>{props.price}</span>
+         </div>
+      </div>       
     );
 }
 
@@ -81,16 +104,7 @@ function Header() {
     );
 }
 
-function Menu() {
-    return (
-        <main className="menu">
-            <h2>Our Menu</h2>
-            <Pizza />
-            <Pizza />
-            <Pizza />
-        </main>
-    );
-}
+
 
 function Footer() {
     const hour = new Date().getHours();
