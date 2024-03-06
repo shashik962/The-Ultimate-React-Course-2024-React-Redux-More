3b5980b15631d15628045fc24aeed7e089633fb4
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-06 23:05:40 +0530

    52. React Fragments

--- src/index.js
+++ src/index.js
@@ -67,11 +67,17 @@ function Menu() {
         <main className="menu">
             <h2>Our Menu</h2>
             {numPizzas > 0 ? (
-                <ul className="pizzas">
-                  {pizzaData.map(pizza => (
-                     <Pizza pizzaObj={pizza} key={pizza.name} />
-                  ))}
-               </ul>
+               <>
+                  <p>
+                     Authentic italian cuisine. 6 creative dishes to choose from. All from stone even, all organic, all delicious.
+                  </p>
+                  <ul className="pizzas">
+                     {pizzaData.map(pizza => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                     ))}
+                  </ul>
+               </>
+              
             ): (
                <p>We're still working on our menu. Please come back later :) </p>
             )}
