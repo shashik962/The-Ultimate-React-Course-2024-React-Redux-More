3aea38c0a0696a278cd7f91d34b3f718e300f689
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-09 11:34:58 +0530

    71. Rendering the Items List

--- steps/travel-list/src/App.js
+++ steps/travel-list/src/App.js
@@ -1,3 +1,10 @@
+
+const initialItems  = [
+    { id: 1, description: "Passport", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+    { id: 3, description: "Charger", quantity: 12, packed: false },
+];
+
 export default function App() {
     return (
         <div className="app">
@@ -24,8 +31,23 @@ function Form() {
 function PackingList() {
     return (
         <div className="list">
-            List
+            <ul>
+              {initialItems.map(item=>(
+                <Item item={item} />
+              ))}
+            </ul> 
         </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through"  } : {}}>
+              {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
     );
 }
 
