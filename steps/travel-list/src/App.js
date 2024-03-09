779a8da730ba822c0d0d790166aa6a0a1210ff1a
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-09 18:57:56 +0530

    80. Thinking About State and Lifting State Up

--- steps/travel-list/src/App.js
+++ steps/travel-list/src/App.js
@@ -1,17 +1,23 @@
 import { useState } from "react";
 
-const initialItems  = [
-    { id: 1, description: "Passport", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: true },
-    { id: 3, description: "Charger", quantity: 12, packed: false },
-];
+// const initialItems  = [
+//     { id: 1, description: "Passport", quantity: 2, packed: false },
+//     { id: 2, description: "Socks", quantity: 12, packed: true },
+//     { id: 3, description: "Charger", quantity: 12, packed: false },
+// ];
 
 export default function App() {
+    const [items, setItems] = useState([]);
+
+    function handleAddItems(item) {
+        setItems((items) => [...items, item]);
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList items={items} />
             <Stats />
         </div>
     );
@@ -21,7 +27,7 @@ function Logo() {
     return <h1>🌴 Far aways 💼</h1>
 }
 
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] =  useState(1);
 
@@ -31,7 +37,9 @@ function Form() {
       if(!description) return;
 
       const newItem = {description, quantity, packed: false, id: Date.now()};
-      console.log(newItem)
+      console.log(newItem);
+      onAddItems(newItem);  
+
       setDescription("");
       setQuantity(1);
     }
@@ -51,11 +59,11 @@ function Form() {
     );
 }
 
-function PackingList() {
+function PackingList({ items }) {
     return (
         <div className="list">
             <ul>
-              {initialItems.map(item=>(
+              {items.map(item=>(
                 <Item item={item} key={item.id} />
               ))}
             </ul> 
