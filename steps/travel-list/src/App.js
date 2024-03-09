1929ab4b8f2e897dbe6554d2063dadf75a7995bb
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-09 21:13:36 +0530

    87. Clearing the List

--- steps/travel-list/src/App.js
+++ steps/travel-list/src/App.js
@@ -27,11 +27,16 @@ export default function App() {
         );
     }
 
+    function handleClearList() {
+        const confirm = window.confirm("Are you sure you want to delete all items?");
+        if(confirm) setItems([]);
+    }
+
     return (
         <div className="app">
             <Logo />
             <Form onAddItems={handleAddItems} />
-            <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handletoggleItem} />
+            <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handletoggleItem} onClearList={handleClearList} />
             <Stats items={items} />
         </div>
     );
@@ -73,7 +78,7 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
     const [sortBy, setSortBy] = useState("input");
 
     let sortedItems;
@@ -103,6 +108,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
                     <option value="description" >Sort by description</option>
                     <option value="packed" >Sort by packed status</option>
                 </select>
+                <button onClick={onClearList}>Clear list</button>
             </div>
         </div>
     );
