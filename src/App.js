7f721d49c7d8e9ebbe586b2a57c58ac3afc3fac9
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-10 14:33:42 +0530

    112. Fixing Prop Drilling With Composition (And Building a Layout)

--- src/App.js
+++ src/App.js
@@ -57,19 +57,26 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies}/>
-      <Main movies={movies} />
+      <NavBar> 
+          <Search />
+          <NumResults movies={movies} />
+      </NavBar>
+      <Main >
+        <ListBox  > 
+           <MovieList movies={movies} /> 
+        </ListBox>
+        <WatchBox />
+      </Main> 
     </>
   );
 }  
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
    return (
-    <nav className="nav-bar">
-      <Logo />
-      <Search />
-      <NumResults movies={movies} />
-    </nav>
+      <nav className="nav-bar">
+        <Logo />
+        {children}
+      </nav>  
    );
 } 
 
@@ -104,16 +111,15 @@ function Search() {
     );
 }
 
-function Main({ movies }) {
+function Main({ children }) {
     return (
       <main className="main">
-        <ListBox movies={movies} />
-        <WatchBox />
+          {children}
       </main>
     );
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
    return (
     <div className="box">
@@ -123,7 +129,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} /> }
+      {isOpen1 && children }
     </div>
    );
 }
