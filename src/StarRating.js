89feb82bd39828315f2c7d6af6ea2fa010a0e9a9
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-10 20:03:06 +0530

    117. Handling Hover Events

--- src/StarRating.js
+++ src/StarRating.js
@@ -16,7 +16,8 @@ const textStyle = {
 }
 
 export default function StarReating({ maxRating = 5 }) {
-   const [rating, setRating] = useState(1);
+   const [rating, setRating] = useState(0);
+   const [tempRating, setTempRating] = useState(0);
 
    function handleRating(rating) {
       setRating(rating);
@@ -28,12 +29,14 @@ export default function StarReating({ maxRating = 5 }) {
             {Array.from({length: maxRating}, (_, i) => (
                <Star 
                   key={i} 
+                  full={tempRating ? tempRating >= i +1 : rating >= i + 1}
                   onRate={() => handleRating(i + 1)} 
-                  full={rating >= i + 1}
+                  onHoverIn={() => setTempRating(i + 1)}
+                  onHoverOut={() => setTempRating(i + 1)}
                />
             ))}
          </div>
-         <p style={textStyle}>{rating || "" }</p>
+         <p style={textStyle}>{tempRating || rating || "" }</p>
       </div>
    );
 }
@@ -45,9 +48,14 @@ const starStyle = {
    cursor: "pointer"
 }
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
     return (
-         <span role="button" style={starStyle} onClick={onRate}>
+         <span role="button" 
+            style={starStyle} 
+            onClick={onRate} 
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+            >
             {full? (
                <svg
                   xmlns="http://www.w3.org/2000/svg"
