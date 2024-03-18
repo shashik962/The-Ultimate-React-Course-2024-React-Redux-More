4763de2d9475378cbdd58905c539b182a3a31aea
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-18 15:43:51 +0530

    158. CHALLENGE #1: Currency Converter

--- currency-converter/src/index.js
+++ currency-converter/src/index.js
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
   <StrictMode>
     <App />
