9ffc5dc0bd08ddc467466ded5d374c3f5a5c7bf7
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-03-18 12:07:23 +0530

    151. Loading Movie Details

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import StarReating from "./StarRating";
 
 const tempMovieData = [
   {
@@ -273,14 +274,72 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
-    return (
-      <div className="details">
-          <button className="btn-back" onClick={onCloseMovie}>
-              &larr;
-          </button>
-          {selectedId}
-      </div>
-    );
+  const [movie, setMovie] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre 
+  } = movie
+
+  useEffect(function () {
+    async function getMovieDetails() {
+      setIsLoading(true);
+      const res =  await fetch(`http://www.omdbapi.com/?apikey=${key}&i=${selectedId}`);
+      const data = await res.json();
+      console.log(data);
+      setMovie(data);
+      setIsLoading(false);
+    }
+    getMovieDetails();
+  }, [selectedId])
+
+  return (
+    <div className="details">
+      {isLoading ? (
+        <Loader />
+      ):(
+        <>
+           <header>
+              <button className="btn-back" onClick={onCloseMovie}>
+                  &larr;
+              </button>
+              <img  src={poster} alt={`Poster of ${movie} movie`}/>
+              <div className="details-overview" >
+                <h2>{title}</h2>
+                <p>
+                  {released} &bull; {runtime}
+                </p>
+                <p>{genre}</p>
+                <p>
+                  <span>⭐️</span>
+                  {imdbRating} IMDb Rating
+                </p>
+              </div>
+            </header>
+            <section>
+              <div className="rating">
+                <StarReating maxRating={10} size={24} />
+              </div>
+              <p>
+                <em>{plot}</em>
+              </p>
+              <p>Starring {actors}</p>
+              <p>Directed by {director}</p>
+            </section>
+        </>
+      )}
+     
+    </div>
+  );
 }
 
 function WatchSummary({ watched }) {
