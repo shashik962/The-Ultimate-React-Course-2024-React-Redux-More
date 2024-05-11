499cc1e09ebf4854a5ce5ca1a328325fd0464216
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-05-11 21:10:58 +0530

    267. Back to React! Connecting our Redux App With React

--- section-20/redux-intro/src/features/customers/Customer.js
+++ section-20/redux-intro/src/features/customers/Customer.js
@@ -1,4 +1,11 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
+
+  const customer = useSelector( store => store.customer.fullName);
+
+  console.log(customer);
+
   return <h2>👋 Welcome, %NAME%</h2>;
 }
 
